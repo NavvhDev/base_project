aad631eeb17006565c3b5f3898ca4c2c8a163ed1
Ivan Albarracin <ivanalbarracin00@hotmail.com>
2023-12-17 16:16:47 -0300

    nuevo

--- MiCV/MiCV/asgi.py
+++ MiCV/MiCV/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Proyecto_MiCV project.
+ASGI config for MiCV project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Proyecto_MiCV.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MiCV.settings')
 
 application = get_asgi_application()
