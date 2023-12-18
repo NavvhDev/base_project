0c84243bb0d1cc6d1b1e4a885e72547e2f9bc5a9
Ivan Albarracin <ivanalbarracin00@hotmail.com>
2023-12-18 20:37:48 -0300

    update 1

--- MiCV/MiCV/urls.py
+++ MiCV/MiCV/urls.py
@@ -20,5 +20,9 @@ from tasks import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.helloworld)
+    path('',views.home, name="home"),
+    path('signup/', views.signup, name='signup'),
+    path('tasks/', views.tasks, name='tasks'),
+    path('logout/', views.signout, name='logout'),
+    path('signin/', views.signin, name='signin'),
 ]
